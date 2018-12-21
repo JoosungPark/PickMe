9ce3311fa40d2dadbaae9b702ee9f7b7d542ff47
joosung.park <sdop25@gmail.com>
2018-12-21 19:00:23 +0900

    starred fragment stub

---
+++ app/src/main/java/com/joosung/pickme/ui/starred/StarredFragment.kt
@@ -0,0 +1,10 @@
+package com.joosung.pickme.ui.starred
+
+import com.joosung.pickme.common.BaseFragment
+
+class StarredFragment : BaseFragment() {
+
+    companion object {
+        fun newInstance() = StarredFragment()
+    }
+}
