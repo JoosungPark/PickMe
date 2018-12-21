7a9736874ec8b494b874171cd171164fc2511b53
joosung.park <sdop25@gmail.com>
2018-12-21 19:00:08 +0900

    search view model

---
+++ app/src/main/java/com/joosung/pickme/ui/search/SearchViewModel.kt
@@ -0,0 +1,245 @@
+package com.joosung.pickme.ui.search
+
+import android.content.Context
+import android.databinding.ObservableField
+import com.joosung.library.rx.RxUtils
+import com.joosung.library.rx.RxViewModel
+import com.joosung.library.rx.Variable
+import com.joosung.library.vm.SingleLiveEvent
+import com.joosung.pickme.R
+import com.joosung.pickme.common.AppServerInterface
+import com.joosung.pickme.common.MediaRepository
+import com.joosung.pickme.http.api.GetImageRequest
+import com.joosung.pickme.http.api.GetVideoRequest
+import com.joosung.pickme.http.model.MediaUrl
+import com.joosung.pickme.http.model.SharedMedia
+import com.joosung.rxrecycleradapter.RxRecyclerAdapterChangeEvent
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.PublishSubject
+import java.util.ArrayList
+
+class SearchViewModel(
+        private val input: SearchViewModelInput,
+        private val repo: MediaRepository,
+        private val service: MediaServerInterface
+) : RxViewModel() {
+
+    private val mediaSource = Variable(arrayListOf<MediaUrl>())
+    fun getMediaSource() = mediaSource
+
+    val dataSourceSubject = PublishSubject.create<RxRecyclerAdapterChangeEvent<SearchCellType>>()
+    private val errorEvent = SingleLiveEvent<String>()
+    fun getErrorEvent() = errorEvent
+
+    val isLoading = ObservableField<Boolean>()
+    private var latestItemCount = 0
+
+    private val tapImageEvent = SingleLiveEvent<Int>()
+    fun getTapImageEvent() = tapImageEvent
+
+    private val isRefreshingEvent = SingleLiveEvent<Boolean>()
+    fun getIsRefreshingEvent() = isRefreshingEvent
+
+    private val defaultSize = repo.getDefaultCount()
+    private var page = 1
+    private var latestQuery: String? = null
+
+    val isEdit = ObservableField(false)
+    private val checkedMedias = arrayListOf<MediaUrl>()
+    private val uncheckedMedias = arrayListOf<MediaUrl>()
+
+    fun monitor() {
+        launch {
+            mediaSource
+                    .asObservable()
+                    .distinctUntilChanged()
+                    .subscribe {
+                        val previous = latestItemCount - 1
+                        dataSourceSubject.takeIf { !it.hasComplete() }
+                                ?.onNext(RxRecyclerAdapterChangeEvent.Removed(previous))
+                        val cells = arrayListOf<SearchCellType>()
+                        cells.addAll(it.filterIndexed { index, _ -> index >= previous }.map { SearchCellType.Media(it, repo) })
+                        cells.add(SearchCellType.Next)
+
+                        dataSourceSubject.takeIf { !it.hasComplete() }
+                                ?.onNext(RxRecyclerAdapterChangeEvent.InsertedRange(previous, cells))
+                        latestItemCount = previous + cells.size
+                    }
+        }
+    }
+
+    fun init() {
+        val cells = arrayListOf(SearchCellType.Description(input.description))
+        dataSourceSubject.takeIf { !it.hasComplete() }?.onNext(RxRecyclerAdapterChangeEvent.Reloaded(cells))
+    }
+
+    fun queryMedia(query: String? = null) {
+        val queryNonNull = query ?: latestQuery
+
+        queryNonNull?.also {
+            launch {
+                latestItemCount = 0
+                mediaSource.value.clear()
+                isLoading.set(true)
+                page = 1
+                latestQuery = it
+
+                service.queryMedia(it, page, defaultSize)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeBy(
+                                onSuccess = { urlList ->
+                                    val cells = arrayListOf<SearchCellType>()
+
+                                    if (urlList.isNotEmpty()) {
+                                        cells.addAll(urlList.map { SearchCellType.Media(it, repo) })
+                                        cells.add(SearchCellType.Next)
+                                        latestItemCount = cells.size
+                                        dataSourceSubject.takeIf { !it.hasComplete() }
+                                                ?.onNext(RxRecyclerAdapterChangeEvent.Reloaded(cells))
+
+                                        mediaSource.value = urlList
+                                    }
+
+                                    isRefreshingEvent.value = false
+                                    isLoading.set(false)
+
+                                },
+                                onError = { error ->
+                                    errorEvent.value = error.message
+                                    isRefreshingEvent.value = false
+                                    isLoading.set(false)
+                                }
+                        )
+            }
+        } ?: kotlin.run {
+            errorEvent.value = input.errorEmptyKeyword
+            isRefreshingEvent.value = false
+        }
+    }
+
+    fun queryNext() {
+        if (++page > 15) {
+            errorEvent.value = "최대 지원 페이지는 15까지입니다."
+            return
+        }
+
+        latestQuery?.also { query ->
+            launch {
+                service.queryMedia(query, page, defaultSize)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeBy(
+                                onSuccess = { imageIdList ->
+                                    dataSourceSubject.takeIf { !it.hasComplete() }
+                                            ?.onNext(RxRecyclerAdapterChangeEvent.Removed(latestItemCount - 1))
+                                    val cells = arrayListOf<SearchCellType>()
+
+                                    if (imageIdList.isNotEmpty()) {
+                                        cells.addAll(imageIdList.map { SearchCellType.Media(it, repo) })
+                                        cells.add(SearchCellType.Next)
+                                        dataSourceSubject.takeIf { !it.hasComplete() }
+                                                ?.onNext(RxRecyclerAdapterChangeEvent.InsertedRange(latestItemCount - 1, cells))
+                                        latestItemCount = latestItemCount - 1 + cells.size
+
+                                        val list = arrayListOf<MediaUrl>()
+                                        list.addAll(mediaSource.value)
+                                        list.addAll(imageIdList)
+                                        mediaSource.value = list
+                                    }
+                                },
+                                onError = { error ->
+                                    errorEvent.value = error.message
+                                }
+                        )
+            }
+        }
+
+    }
+
+    fun tapMedia(url: MediaUrl?) {
+        isEdit.get()?.also { edit ->
+            if (edit) {
+                url?.also { url ->
+                    repo.getMedia(url)?.also { media ->
+                        media.starred.get()?.also {
+                            val target = if (it) uncheckedMedias else checkedMedias
+                            target.add(url)
+                            media.starred.set(!it)
+                        }
+                    }
+                }
+            } else {
+                url?.also { tapImageEvent.value = mediaSource.value.indexOf(it) }
+            }
+        }
+    }
+
+    fun longTapMedia(): Boolean {
+        return if (isEdit.get() == false) {
+            isEdit.set(true)
+
+            true
+        } else {
+
+            false
+        }
+    }
+
+    fun tapCancel() {
+        checkedMedias.clear()
+        uncheckedMedias.clear()
+        isEdit.set(false)
+    }
+
+    fun tapSave() {
+        // TODO: update realm db from checked/unchecked Media list.
+
+        tapCancel()
+    }
+}
+
+interface SearchViewModelInput {
+    val description: String
+    val errorEmptyKeyword: String
+}
+
+class SearchViewModelInputImpl(context: Context) : SearchViewModelInput {
+    override val description: String = context.getString(R.string.Search_Description)
+    override val errorEmptyKeyword: String = context.getString(R.string.Error_Empty_Keyword)
+}
+
+interface MediaServerInterface {
+    fun queryMedia(query: String, page: Int, size: Int): Single<ArrayList<MediaUrl>>
+}
+
+class MediaServer(private val server: AppServerInterface) : MediaServerInterface {
+    override fun queryMedia(query: String, page: Int, size: Int): Single<ArrayList<MediaUrl>> {
+        return Single.create { emitter ->
+            val imageRequest = server.request(GetImageRequest(query, page, size))
+            val videoRequest = server.request(GetVideoRequest(query, page, size))
+
+            RxUtils.zip(imageRequest, videoRequest) { e1, e2 -> Pair(e1, e2) }
+                    .subscribeBy(
+                            onNext = { (imageResponse, videoResponse) ->
+                                if (!emitter.isDisposed) {
+                                    val list = arrayListOf<MediaUrl>()
+                                    val medias = arrayListOf<SharedMedia>()
+
+                                    imageResponse.response.get()?.medias?.also { medias.addAll(it) }
+                                    videoResponse.response.get()?.medias?.also { medias.addAll(it) }
+
+                                    medias.sortWith(Comparator { e1, e2 -> e1.dateTime.time.compareTo(e2.dateTime.time) })
+                                    medias.forEach { list.add(it.thumbnailUrl()) }
+
+                                    emitter.onSuccess(list)
+                                }
+
+                            },
+                            onError = {
+                                emitter.onError(it)
+                            }
+                    )
+        }
+    }
+}
