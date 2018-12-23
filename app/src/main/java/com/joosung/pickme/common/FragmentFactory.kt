5535ac85c1a87c5d5142ceac5499bd51718c58fc
joosung.park <sdop25@gmail.com>
2018-12-23 17:10:13 +0900

    안쓰는 파일 제거.

--- app/src/main/java/com/joosung/pickme/common/FragmentFactory.kt
+++ app/src/main/java/com/joosung/pickme/common/FragmentFactory.kt
@@ -1,14 +1,9 @@
 package com.joosung.pickme.common
 
-import com.joosung.pickme.http.model.MediaUrl
 import com.joosung.pickme.ui.home.HomeFragment
-import com.joosung.pickme.ui.image.ImagePagerFragment
-import com.joosung.pickme.ui.image.item.ImageFragment
 
 sealed class FragmentBundle {
     object Home : FragmentBundle()
-    data class ImagePager(val index: Int) : FragmentBundle()
-    data class Image(val id: MediaUrl) : FragmentBundle()
 }
 
 class FragmentFactory {
@@ -16,8 +11,6 @@ class FragmentFactory {
     companion object {
         fun createFragment(bundle: FragmentBundle): BaseFragment = when (bundle) {
                 is FragmentBundle.Home -> HomeFragment.newInstance()
-                is FragmentBundle.ImagePager -> ImagePagerFragment.newInstance(bundle.index)
-                is FragmentBundle.Image -> ImageFragment.newInstance(bundle.id)
         }
     }
 }
